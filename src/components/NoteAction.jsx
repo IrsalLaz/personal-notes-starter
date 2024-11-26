c8a4b0da9207bc67620aae8c67c5f59ff9ece8d5
Irsal Lazuard Izzatillah <irsallazuard547@gmail.com>
2024-11-26 17:40:21 +0700

    add isArchive for if statement

--- src/components/NoteAction.jsx
+++ src/components/NoteAction.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ArchiveButton from "./ArchiveButton";
 import DeleteButton from "./DeleteButton";
 
-function NoteAction({ id, onDelete, onArchive }) {
+function NoteAction({ id, onDelete, onArchive, isArchive}) {
 	return (
 		<div className="action-button">
 			<DeleteButton />
+			{/* TODO add is archive checking */}
+			{/* {if (!isArchive){} } */}
 			<ArchiveButton />
 		</div>
 	);
