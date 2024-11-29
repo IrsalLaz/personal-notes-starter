4401cd3e5d56395b3f2ba598c39271336ec24fa7
Irsal Lazuard Izzatillah <irsallazuard547@gmail.com>
2024-11-29 14:41:14 +0700

    add empty message in notes container

--- src/components/NotesContainer.jsx
+++ src/components/NotesContainer.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 import NoteCard from "./NoteCard";
 
-function NotesContainer({ notes, onDelete, onArchive }) {
-	return (
-		<div className="notes-list">
-			{/* TODO: change to variable instead */}
-			{notes.map((note) => (
-				<NoteCard
-					key={note.id}
-					id={note.id}
-					title={note.title}
-					createdAt={note.createdAt}
-					body={note.body}
-					isArchive={note.isArchive}
-					onDelete={onDelete}
-					onArchive={onArchive}
-				/>
-			))}
-		</div>
-	);
+function NotesContainer({ notes, onDelete, onArchive, onMove }) {
+	if (notes.length !== 0) {
+		return (
+			<div className="notes-list">
+				{notes.map((note) => (
+					<NoteCard
+						key={note.id}
+						id={note.id}
+						title={note.title}
+						createdAt={note.createdAt}
+						body={note.body}
+						archived={note.archived}
+						onDelete={onDelete}
+						onArchive={onArchive}
+						onMove={onMove}
+					/>
+				))}
+			</div>
+		);
+	} else {
+		return <p className="notes-list__empty-message">Tidak ada catatan.</p>;
+	}
 }
 
 export default NotesContainer;
