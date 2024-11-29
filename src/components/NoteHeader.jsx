c280224c6a94956a8e37091b68b0f73db40e3608
Irsal Lazuard Izzatillah <irsallazuard547@gmail.com>
2024-11-29 14:40:27 +0700

    fix date format using showFormattedDate

--- src/components/NoteHeader.jsx
+++ src/components/NoteHeader.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { showFormattedDate } from "../utils";
 
 function NoteHeader({ title, createdAt }) {
 	return (
 		<div>
 			<h3 className="note-item__title">{title}</h3>
-			<p className="note-item__date">{createdAt}</p>
+			<p className="note-item__date">{showFormattedDate(createdAt)}</p>
 		</div>
 	);
 }
